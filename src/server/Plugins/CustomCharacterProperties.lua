871e30b7a31895b42285e0a52e4094bd30ff855b
vocal <48099292+coolest@users.noreply.github.com>
2023-08-23 20:01:23 -0500

    updated

--- src/server/Plugins/CustomCharacterProperties.lua
+++ src/server/Plugins/CustomCharacterProperties.lua
@@ -16,11 +16,4 @@ return CharacterAdded.Signal:Connect(function(character)
     root.CustomPhysicalProperties = PhysicalProperties.new(35, 0.5, 0.3)
 
     humanoid.JumpPower = 100;
-    humanoid.Jumping:Connect(function(isJumping)
-        if not isJumping then
-            return;
-        end
-
-        root.AssemblyLinearVelocity += Vector3.new(0, 55, 0)
-    end)
 end)
